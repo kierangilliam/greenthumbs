d273e1ff6d881cfec017f034bfce9f81e6f80c76
Kieran Gill <kieran.gill1997@gmail.com>
2020-04-23 21:20:24 -0700

    Fix filenaming

--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -18,18 +18,22 @@ os.mkdir(dst)
 total_files = 0
 
 for dataset in folders:
-	for subfolder in ['ann', 'img']:
 
-		print(dataset)
-		path = os.path.join(os.getcwd(), dataset + '/' + subfolder) 
-		files = [f for f in listdir(path) if isfile(join(path, f))]
-		print(len(files))
-	    
-		# mv path -> dst
-		# rename new files
-		for i, file in enumerate(files):
-			filename = f"{i + total_files}__{file}"
-			print(filename)
-			shutil.copy2(path + "/" + file, dst + "/" + filename)
+	print(dataset)
+	path = os.path.join(os.getcwd(), dataset) 
+	ann_path = os.path.join(path, './ann')
+	print(ann_path)
+	files = [f for f in listdir(ann_path) if isfile(join(ann_path, f))]
+	print(len(files))
+    
+	# mv path -> dst
+	# rename new files
+	for i, file in enumerate(files):
+		src_img_file = file[:-5] 
+		dst_img_file = f"{i + total_files}__{src_img_file}"	
+		filename = f"{i + total_files}__{file}"
+		print(filename)
+		shutil.copy2(ann_path + "/" + file, dst + "/" + filename)
+		shutil.copy2(path + "/img/" + src_img_file, dst + "/" + dst_img_file)
  
 	total_files += len(files)
