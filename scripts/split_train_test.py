2bff7fdd959ca7942569db5c6a50d73115700ea7
Kieran Gill <kieran.gill1997@gmail.com>
2020-04-24 22:00:10 -0700

    Move everything in 'tomatoes' up a level, add variations to train test splits

--- scripts/split_train_test.py
+++ scripts/split_train_test.py
@@ -2,37 +2,38 @@ import random
 import json
 
 # Creates two different coco files out of a single coco file
-base_dir = "data/v01/tomatoes"
+base_dir = "data/v01/"
 
-ratio = .7
+ratios = [.65, .75, .85]
 
 coco_file = base_dir + '/coco.json'
 
 with open(coco_file) as f:
   dataset = json.load(f)
 
-train, test = {}, {}
-train["categories"] = dataset["categories"]
-test["categories"] = dataset["categories"]
+for ratio in ratios:
+    train, test = {}, {}
+    train["categories"] = dataset["categories"]
+    test["categories"] = dataset["categories"]
 
-train_len = int(len(dataset["images"]) * ratio)
-random.shuffle(dataset["images"])
+    train_len = int(len(dataset["images"]) * ratio)
+    random.shuffle(dataset["images"])
 
-train["images"] = dataset["images"][:train_len]
-test["images"] = dataset["images"][train_len:]
+    train["images"] = dataset["images"][:train_len]
+    test["images"] = dataset["images"][train_len:]
 
-for ds in [train, test]:
-  annotations = []
-  for image in ds["images"]:
-    i = image["id"]
-    annotations.extend([ann for ann in dataset["annotations"] if ann["image_id"] == i])
-  ds["annotations"] = annotations
+    for ds in [train, test]:
+      annotations = []
+      for image in ds["images"]:
+        i = image["id"]
+        annotations.extend([ann for ann in dataset["annotations"] if ann["image_id"] == i])
+      ds["annotations"] = annotations
 
-with open(base_dir + '/train_coco.json', 'w') as outfile:
-  json.dump(train, outfile)
+    with open(base_dir + f'/train_{int(ratio * 100)}%_coco.json', 'w') as outfile:
+      json.dump(train, outfile)
 
-with open(base_dir + '/test_coco.json', 'w') as outfile:
-  json.dump(test, outfile)
+    with open(base_dir + f'/test_{int(ratio * 100)}%_coco.json', 'w') as outfile:
+      json.dump(test, outfile)
 
-print(f"Done. {len(train['images'])} train images, {len(test['images'])} test images.")
+    print(f"Done. {len(train['images'])} train images, {len(test['images'])} test images.")
 
