7a38144a7a8df56cc6214298f9256022d47459d9
Kieran Gill <kieran.gill1997@gmail.com>
2020-04-26 19:27:56 -0700

    Add boostrap script for aws instance

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+pip install -U torch==1.4 torchvision==0.5 -f https://download.pytorch.org/whl/cu101/torch_stable.html 
+pip install cython pyyaml==5.1
+pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+gcc --version
+pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/index.html
+
+
