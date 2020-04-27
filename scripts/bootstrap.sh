b98598b08c745ffda12e6198c0b64d758a6c7a68
Kieran Gill <kieran.gill1997@gmail.com>
2020-04-26 19:37:26 -0700

    Add cv dependency to bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,5 +3,5 @@ pip install cython pyyaml==5.1
 pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 gcc --version
 pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/index.html
-
+pip install opencv-python
 
